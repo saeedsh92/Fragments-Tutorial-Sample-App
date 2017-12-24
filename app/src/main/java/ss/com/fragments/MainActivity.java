35bfa2ea134745b8a1325766ae389fac8bd9e644
Saeed Shahini <saeedshahiniit@gmail.com>
2017-12-24 15:08:28 +0330

    Before Start Session

--- app/src/main/java/ss/com/fragments/MainActivity.java
+++ app/src/main/java/ss/com/fragments/MainActivity.java
@@ -1,7 +1,11 @@
 package ss.com.fragments;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
     }
+
+    private void setupViews() {
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        Button sayHelloToFragmentsButton = findViewById(R.id.btn_main_simple);
+        sayHelloToFragmentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SayHelloToFragmentsActivity.class));
+            }
+        });
+
+
+        Button fragmentTransactionButton = findViewById(R.id.btn_main_fragmentTransaction);
+        fragmentTransactionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FragmentTrasactionsActivity.class));
+            }
+        });
+
+        Button viewPagerButton = findViewById(R.id.btn_main_viewPager);
+        viewPagerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ViewPagerActivity.class));
+            }
+        });
+
+        Button multipaneLayoutButton = findViewById(R.id.btn_main_multipaneLayout);
+        multipaneLayoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MultipaneLayoutActivity.class));
+            }
+        });
+    }
+
 }
